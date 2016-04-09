84eff7db6bc71188312b07f3b1877f005fa8d21f
Uritomi <bolecki019@gmail.com>
2016-04-09 11:12:20 -0400

    remove pyc files and add index

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Item
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the lists index.")
+    items = Item.objects.all()
+    output = "<br>".join([i.list_text for i in items])
+    return HttpResponse(output)

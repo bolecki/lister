06745fd1d3e3b1e9771ac67e03c2b95e5234ee90
Uritomi <bolecki019@gmail.com>
2016-04-09 11:24:17 -0400

    use render instead of template and HttpResponse

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from django.template import loader
 from .models import Item
 
 def index(request):
     items = Item.objects.all()
-    template = loader.get_template('lists/index.html')
-    context = {
-        'items': items,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'items': items}
+    return render(request, 'lists/index.html', context)

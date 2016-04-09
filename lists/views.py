3c87ad66aa3562dd35cb19e4d9da8a7954bc7795
Uritomi <bolecki019@gmail.com>
2016-04-09 13:33:48 -0400

    add post method to add list items

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from .models import Item
 
 def index(request):
-    items = Item.objects.all()
-    context = {'items': items}
-    return render(request, 'lists/index.html', context)
+    if request.method == 'GET':
+        items = Item.objects.all()
+        context = {'items': items}
+        return render(request, 'lists/index.html', context)
+    elif request.method == 'POST':
+        i = Item(list_text=request.POST['add'])
+        i.save()
+        return HttpResponseRedirect(reverse('lists:index'))

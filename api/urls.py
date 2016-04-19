cc905991ab4ed0299eda087a68907866c6ac2bd3
Uritomi <bolecki019@gmail.com>
2016-04-19 17:19:02 -0400

    api url path take random before options

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'api'
 urlpatterns = [
     url(r'^v1/$', views.index, name='index'),
-    url(r'^v1/(?P<option>[a-z]+)/random/$', views.random, name='random'),
+    url(r'^v1/random/(?P<option>[a-z]+)/$', views.random, name='random'),
 ]

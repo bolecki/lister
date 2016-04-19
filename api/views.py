999245ab3fe0e4e75c4a58ac657f3e7ce60e3e23
Uritomi <bolecki019@gmail.com>
2016-04-18 21:58:19 -0400

    add random pickers to api

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.core import serializers
+from django.db.models import Max
 
 from lists.models import Item
 
@@ -9,3 +10,14 @@ def index(request):
     data = serializers.serialize("json", items)
     
     return HttpResponse(data);
+
+def random(request, option):
+    if option == "tie":
+        max_votes = Item.objects.all().aggregate(Max('votes'))['votes__max']
+        items = Item.objects.filter(votes=max_votes)
+        item = items.order_by('?').first()
+        return HttpResponse(item)
+    else:
+        items = Item.objects.all().order_by('-votes')
+        item = items.order_by('?').first()
+        return HttpResponse(item)

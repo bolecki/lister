a3416d2d1c3885c442d894f541d54478fcdaed88
Uritomi <bolecki019@gmail.com>
2016-04-19 17:14:29 -0400

    add version to api path

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'api'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<option>[a-z]+)/random/$', views.random, name='random'),
+    url(r'^v1/$', views.index, name='index'),
+    url(r'^v1/(?P<option>[a-z]+)/random/$', views.random, name='random'),
 ]

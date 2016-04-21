656d7544fbe8ada5862da4ab92a29fcea8bba44b
Uritomi <bolecki019@gmail.com>
2016-04-21 17:00:27 -0400

    using list_id with new model and added login page at index

--- lister/lists/views.py
+++ lister/lists/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-from .models import Item
+from .models import Lister, Item
 
 def index(request):
     if request.method == 'GET':
         items = Item.objects.all().order_by('-votes')
         context = {'items': items}
 
-        return render(request, 'lists/index.html', context)
+        return render(request, 'lists/login.html', context)
 
     elif request.method == 'POST':
         text = request.POST['add']
@@ -19,6 +19,25 @@ def index(request):
             i.save()
 
         return HttpResponseRedirect(reverse('lists:index'))
+
+
+def lister(request, list_id):
+    lister = Lister.objects.get(pk=list_id)
+
+    if request.method == 'GET':
+        items = lister.item_set.all().order_by('-votes')
+        context = {'items': items}
+
+        return render(request, 'lists/index.html', context)
+
+    elif request.method == 'POST':
+        text = request.POST['add']
+
+        if text != "":
+            lister.item_set.create(item_text=text, votes=0)
+
+        return HttpResponseRedirect(reverse('lists:index'))
+
 
 def vote(request, item_id, action):
     item = get_object_or_404(Item, pk=item_id)

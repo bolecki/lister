b40f11c73b0ce92cce9dbabc1dca3c6717cf5c73
Uritomi <bolecki019@gmail.com>
2016-04-24 15:07:51 -0400

    added link to logout

--- lister/lists/urls.py
+++ lister/lists/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 
 from . import views
 
 app_name = 'lists'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^logout/$', logout, {'next_page':'/lists'}, name='logout'),
     url(r'^(?P<list_id>[0-9]+)/$', views.lister, name='lister'),
     url(r'^(?P<list_id>[0-9]+)/(?P<item_id>[0-9]+)/(?P<action>[a-z]+)/vote/$', views.vote, name='vote'),
 ]
--- lister/lists/views.py
+++ lister/lists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from django.contrib.auth.models import User
 from .models import Lister, Item

543328c1217685104e2b5a03c1f3520d53968403
Uritomi <bolecki019@gmail.com>
2016-04-25 18:08:58 -0400

    add public boolean to list model

--- lister/lists/models.py
+++ lister/lists/models.py
@@ -10,6 +10,7 @@ from django.contrib.auth.models import User
 class Lister(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=4)
     list_name = models.CharField(max_length=200)
+    public = models.BooleanField(default=False)
 
     def __str__(self):
         return self.list_name

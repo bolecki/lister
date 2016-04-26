fcebf80d642e08c382167138cb45a1379a4138bc
Uritomi <bolecki019@gmail.com>
2016-04-26 19:52:39 -0400

    format create list form as table

--- lister/lists/forms.py
+++ lister/lists/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class CreateListForm(forms.Form):
-    name = forms.CharField(label='List Name', max_length=100)
+    name = forms.CharField(label='Name', max_length=100)
     public = forms.BooleanField(required=False)

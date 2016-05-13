05f3434780fe2d697fc9e91c5703eefb62e598cf
Uritomi <bolecki019@gmail.com>
2016-05-12 21:04:51 -0400

    switch list loading to jquery post calls to reduce traffic

--- lister/lists/urls.py
+++ lister/lists/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^my-lists/$', views.user_lists, name='user_lists'),
     url(r'^create/$', views.create, name='create'),
     url(r'^(?P<list_id>[0-9]+)/$', views.lister, name='lister'),
+    url(r'^part/(?P<list_id>[0-9]+)/$', views.part, name='part'),
     url(r'^(?P<list_id>[0-9]+)/grant/$', views.grant, name='grant'),
     url(r'^(?P<list_id>[0-9]+)/(?P<item_id>[0-9]+)/(?P<action>[a-z]+)/vote/$', views.vote, name='vote'),
 ]

b1b95170cd01db82793197a6078312c86f292cef
Uritomi <bolecki019@gmail.com>
2016-05-13 22:57:11 -0400

    switch pre_build to bash that calls python

---
+++ .openshift/action_hooks/gen_key.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import string
+import random
+import os
+
+repo = os.environ['OPENSHIFT_REPO_DIR']
+path = repo + "/lister/lister/secrets.py"
+
+# Get ascii Characters numbers and punctuation (minus quote characters as they could terminate string).
+chars = ''.join([string.ascii_letters, string.digits, string.punctuation]).replace('\'', '').replace('"', '').replace('\\', '')
+
+SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
+
+with open(path, 'wb') as file:
+    file.write("SECRET_KEY = '{key}'".format(key=SECRET_KEY))

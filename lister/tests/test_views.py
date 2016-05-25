8f623b6fac944fdab0cba42c43aa7fb88f189844
Uritomi <bolecki019@gmail.com>
2016-05-24 21:09:42 -0400

    add test cases for index_part

--- lister/tests/test_views.py
+++ lister/tests/test_views.py
@@ -18,8 +18,6 @@ class TestUser(object):
 class IndexTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username='tester', password='top_secret')
 
     def test_authenticated_index(self):
         request = self.factory.get('/lists')
@@ -36,3 +34,29 @@ class IndexTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertInHTML('<input id="id_sortable" name="sortable" type="hidden" value="False" />', response.content)
+
+
+class IndexPartTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username="user", password="testpass")
+        Lister.objects.create(list_name="public", public=True, user=self.user)
+        Lister.objects.create(list_name="private", user=self.user)
+
+    def test_public_index_part(self):
+        request = self.factory.get('/lists/index-part/public')
+        request.user = self.user
+        response = lists.views.index_part(request, 'public')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertInHTML('<div>public<small> - user</small></div>', response.content)
+        self.assertInHTML('<div>private<small></small></div>', response.content, count=0)
+
+    def test_private_index_part(self):
+        request = self.factory.get('/lists/index-part/mylists')
+        request.user = self.user
+        response = lists.views.index_part(request, 'mylists')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertInHTML('<div>private<small></small></div>', response.content)
+        self.assertInHTML('<div>public<small> - user</small></div>', response.content, count=0)
